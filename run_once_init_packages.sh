90042eb91037076c274096de49c455dde8b41076
Hunor Batori <batorihunor@gmail.com>
2025-01-06 10:27:21 +0100

    Replace $USER_HOME tags with ~ as the script should not ran as root anymore. Put sudo where it was missing

--- run_once_init_packages.sh
+++ run_once_init_packages.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
+green_echo() {
+    echo -e "\e[32m$1\e[0m"
+}
+
 NNN_BINARY_URL=https://github.com/jarun/nnn/releases/download/v5.0
 NNN_BINARY_ARCHIVE=nnn-nerd-static-5.0.x86_64.tar.gz
 
-# Ensure the script runs as root or with sudo
-# if [[ "$EUID" -ne 0 ]]; then
-#  echo "Please run as root or use sudo."
-#  exit 1
-# fi
+green_echo "Starting system initialization..."
 
-# Ensure the script uses the calling user's home directory
-USER_HOME=$(eval echo "~$SUDO_USER")
-cd "$USER_HOME"
-
-echo "Starting system initialization..."
 
 # Update and upgrade system packages
-echo "Updating and upgrading system packages..."
+green_echo "Updating and upgrading system packages..."
 sudo apt update && apt upgrade -y
 
 # Install essential tools
@@ -26,15 +21,15 @@ ESSENTIAL_TOOLS=(
   tmux
 )
 
-echo "Installing essential tools: ${ESSENTIAL_TOOLS[*]}"
+green_echo "Installing essential tools: ${ESSENTIAL_TOOLS[*]}"
 sudo apt install -y "${ESSENTIAL_TOOLS[@]}"
 
-mkdir -p $USER_HOME/.config/tmux/
-touch $USER_HOME/.config/tmux/tmux.conf
-tmux source $USER_HOME/.config/tmux/tmux.conf
+mkdir -p ~/.config/tmux/
+touch ~/.config/tmux/tmux.conf
+tmux source ~/.config/tmux/tmux.conf
 
-echo "Setting zsh as the default shell"
-sudo chsh -s $(which zsh) "$SUDO_USER"
+green_echo "Setting zsh as the default shell"
+chsh -s $(which zsh)
 
 # Install development tools
 DEV_TOOLS=(
@@ -64,42 +59,41 @@ UTILITIES=(
   man
 )
 
-echo "Installing utilities: ${UTILITIES[*]}"
+green_echo "Installing utilities: ${UTILITIES[*]}"
 sudo apt install -y "${UTILITIES[@]}"
 
-echo "Symlinking batcat to bat"
-ln -s /usr/bin/batcat /usr/bin/bat
+green_echo "Symlinking batcat to bat"
+sudo ln -s /usr/bin/batcat /usr/bin/bat
 
-echo "Installing nnn from static binary"
+green_echo "Installing nnn from static binary"
 wget -P /tmp "${NNN_BINARY_URL}/${NNN_BINARY_ARCHIVE}"
 tar -xzf /tmp/${NNN_BINARY_ARCHIVE} -C /tmp
 mv /tmp/nnn-nerd-static /tmp/nnn
-mv /tmp/nnn /usr/local/bin/
+sudo mv /tmp/nnn /usr/local/bin/
 
-echo "Downloading nnn plugins"
-sh -c "curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | HOME=/home/$SUDO_USER sh"
+green_echo "Downloading nnn plugins"
+sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
 
-
-echo "Installing zsh plugins and theme"
+green_echo "Installing zsh plugins and theme"
 # mkdir -p ~/.zsh/powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $USER_HOME/.zsh/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
 
 # mkdir -p ~/.zsh/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $USER_HOME/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
 # mkdir -p ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions $USER_HOME/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 # mkdir -p ~/.zsh/zsh-completions
-git clone https://github.com/zsh-users/zsh-completions.git $USER_HOME/.zsh/zsh-completions
+git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
 
 # mkdir -p ~/.zsh/fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git $USER_HOME/.zsh/fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.zsh/fzf
 
-$USER_HOME/.zsh/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-zsh --no-fish
+~/.zsh/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-zsh --no-fish
 
 # mkdir -p ~/.zsh/fzf-tab
-git clone https://github.com/Aloxaf/fzf-tab $USER_HOME/.zsh/fzf-tab
+git clone https://github.com/Aloxaf/fzf-tab ~/.zsh/fzf-tab
 
 
 # Configure Git (Optional)
@@ -114,4 +108,4 @@ echo "Cleaning up..."
 apt autoremove -y
 apt autoclean -y
 
-echo "Initialization complete! Please reboot the system if necessary."
+green_echo "Initialization complete! Please reboot the system if necessary."
